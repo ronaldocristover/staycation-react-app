f10060ab706ad40272e004edde8ebd1222d9f3d2
Ronaldo Cristover <ronaldochristover@gmail.com>
2020-10-02 21:38:42 +0700

    Add Category Section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Header from 'parts/Header'
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import landingPage from "../json/landingPage.json";
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 export default class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.refMostPicked = React.createRef();
+    }
+
     render() {
         return (
             <>
-            <Header {...this.props} ></Header>
+                <Header {...this.props}></Header>
+                <Hero
+                    refMostPicked={this.refMostPicked}
+                    data={landingPage.hero}
+                />
+                <MostPicked
+                    refMostPicked={this.refMostPicked}
+                    data={landingPage.mostPicked}
+                    {...this.props}
+                ></MostPicked>
+                <Categories data={landingPage.categories} />
             </>
-        )
+        );
     }
 }
